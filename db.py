6f15e1f608006b3821f8d1ca7594104c9c0bbb09
crgonzalezf <31080616+crgonzalezf@users.noreply.github.com>
2025-01-04 16:59:13 -0300

    Update db.py

--- db.py
+++ db.py
@@ -16,6 +16,6 @@ db = SQLAlchemy(model_class=Base)
 def db_config(app):
     turso_database_url = os.environ.get("TURSO_DATABASE_URL")
     turso_auth_token = os.environ.get("TURSO_AUTH_TOKEN")
-    db_url = f"sqlite+wss{turso_database_url}/?authToken={turso_auth_token}&secure=true"
+    db_url = f"{turso_database_url}/?authToken={turso_auth_token}&secure=true"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     db.init_app(app)

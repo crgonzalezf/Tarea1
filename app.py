ddeec2cc573c9c47161d36a5e96f71ee9fed162a
crgonzalezf <31080616+crgonzalezf@users.noreply.github.com>
2025-01-05 19:29:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap5
 from openai import OpenAI
 from dotenv import load_dotenv
